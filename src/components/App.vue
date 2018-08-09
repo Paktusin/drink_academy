cd77ea9ad4c69347851e919aebd9d45dd521782b
davlichin.ma <davlichin.ma@talantiuspeh.ru>
2018-08-09 12:20:30 +0300

    fix

--- src/components/App.vue
+++ src/components/App.vue
@@ -21,7 +21,7 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="blackboard">
-                                <div class="px-3 pb-2">
+                                <div class="p-2">
                                     <Drink v-if="randomDrinks.length > 0" v-bind:drink="randomDrinks[0]"/>
                                 </div>
                             </div>
